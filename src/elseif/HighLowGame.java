052bb69bbb5299f8b526e1b06a9424fd7b6ece5a
League <league@WTS9.attlocal.net>
2019-01-26 11:20:12 -0800

    Module 3

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -11,28 +11,61 @@ public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
+		
+		
+      for(int i=0;i<10;i++) {		 
+		
 		int random = new Random().nextInt(100);
 		
 		// 2. Print out the random variable above
+		
+		
+		
+		
 		System.out.println(random);
 		// 11. Repeat steps 1 to 10 ten times
 		
 			// 1. Ask the user for a guess using a pop-up window, and save their response 
-     String answer =         JOptionPane.showInputDialog("Guess what the number is");
+    
+
+		
+		
+		
+		String answer =         JOptionPane.showInputDialog("Guess what the number is");
 			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-		int num =       Integer.parseInt(answer);
+		
+     
+     
+     int num =       Integer.parseInt(answer);
 			// 5. if the guess is correct
-				if(num==random) {
+				
+		
+		
+		
+		if(num==random) {
 					JOptionPane.showMessageDialog(null, "You win");
-				}
+				System.exit(num);
+		}
 		// 6. Win
 				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
-			// 7. if the guess is high
-			if(num> random) {
+			
+		// 7. if the guess is high
+		
+		 
+		
+		if(num> random) {
 				JOptionPane.showMessageDialog(null, "Your answer is too high");
-			}
+                  
+				JOptionPane.showMessageDialog(null, "You lose");
+		}
 				// 8. Tell them it's too high
+			if(num<random) {
+				JOptionPane.showMessageDialog(null, "Your answer is too low");
+				
+				JOptionPane.showMessageDialog(null, "You lose");
+			}
 			// 9. if the guess is low
+      }
 				// 10. Tell them it's too low
 
 		// 13. Tell them they lose
---
+++ src/elseif/horoscope.java
@@ -0,0 +1,30 @@
+package elseif;
+
+import javax.swing.JOptionPane;
+
+public class horoscope extends HighLowGame {
+
+public static void main(String[] args) {
+
+
+
+	String Horoscope =  JOptionPane.showInputDialog("What is your star sign");
+	
+	
+	
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}	
+	
+
