4f2e8ac46ed336ac1a0e942f7ac52601277fd068
League <league@WTS9.attlocal.net>
2019-02-16 11:28:48 -0800

    Module 3

--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -13,12 +13,16 @@ public class WalkOfFame {
 		Robot rob = new Robot();
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-           rob.setX( 350);
+          
 		// 2. Make the robot draw a star shape. Hint: 144.
-            rob.setPenColor(Color.blue);
-            
+          drawStar(70, 70, rob);
+          drawStar(160, 160, rob);
+          drawStar(250, 250, rob);
+          drawStar(340,340, rob);
+          drawStar(430, 430, rob);
+          drawStar(520,520, rob);
 		// 3. Set the size of the star to 30.
-           rob.setPenWidth(30);
+         
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of stars like this:
@@ -28,5 +32,19 @@ public class WalkOfFame {
 		 */
 
 	}
-
+public static void drawStar(int x, int y, Robot rob) {
+	rob.setX(x);
+    rob.setY(y);
+    for (int i = 0; i < 5; i++) {
+			
+   		
+        rob.setPenWidth(10);
+        rob.setSpeed(100);
+        rob.setRandomPenColor();
+        rob.penDown();
+        rob.turn(144);
+        rob.move(150);
+        }
 }
+}
+
